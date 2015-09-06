f669d7d5f44df6ed1158adb42b85472409b67aab
Renato Golia <kralizek@gmail.com>
2015-09-06 21:06:45 +0200

    Added CastleWindsor project

---
+++ src/CastleWindsor/Container/WindsorBusContainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.MicroKernel;
+
+namespace Nybus.Container
+{
+    public class WindsorBusContainer : IContainer
+    {
+        private readonly IKernel _kernel;
+
+        public WindsorBusContainer(IKernel kernel)
+        {
+            if (kernel == null) throw new ArgumentNullException(nameof(kernel));
+
+            _kernel = kernel;
+        }
+
+        public T Resolve<T>()
+        {
+            return _kernel.Resolve<T>();
+        }
+
+        public void Release<T>(T component)
+        {
+            _kernel.ReleaseComponent(component);
+        }
+    }
+}
