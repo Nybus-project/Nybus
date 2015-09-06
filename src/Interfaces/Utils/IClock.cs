aa26aeb0c7d911909a1d2cc7619278b72902b49b
Renato Golia <kralizek@gmail.com>
2015-09-06 22:36:46 +0200

    Added Clock to Interfaces

---
+++ src/Interfaces/Utils/IClock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nybus.Utils
+{
+    public static class Clock
+    {
+        static Clock()
+        {
+            Default = new SystemClock();
+        }
+        public static IClock Default { get; set; }
+    }
+
+    public interface IClock
+    {
+        DateTimeOffset Now { get; }
+    }
+
+    public class SystemClock : IClock
+    {
+        public DateTimeOffset Now { get; } = DateTimeOffset.Now;
+    }
+}
