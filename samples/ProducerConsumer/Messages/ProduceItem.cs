bdd293ac333b5834e41cadc930a43ab2ae10d634
Renato Golia <kralizek@gmail.com>
2015-09-07 01:55:34 +0200

    Added Producer-Consumer sample

---
+++ samples/ProducerConsumer/Messages/ProduceItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nybus;
+
+namespace Messages
+{
+    public class ProduceItem : ICommand
+    {
+        public Guid ItemId { get; set; }
+
+        public float Quantity { get; set; }
+
+
+    }
+
+    public class ItemProduced : IEvent
+    {
+        public Guid ItemId { get; set; }
+
+        public float Quantity { get; set; }
+    }
+}
---
+++ samples/ProducerConsumer/Producer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Windsor;
+using MassTransit.BusConfigurators;
+using Messages;
+using Nybus;
+using Nybus.Configuration;
+using Nybus.Container;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var container = CreateContainer())
+            {
+                var bus = CreateBus(container, "rabbitmq://ec2-52-17-96-97.eu-west-1.compute.amazonaws.com/test-1/");
+
+                var handle = bus.Start();
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    InvokeCommand(bus, i);
+                }
+
+                Task.WaitAll(handle.Stop());
+            }
+        }
+
+        private static void InvokeCommand(IBus bus, int i)
+        {
+            var command = new ProduceItem
+            {
+                ItemId = Guid.NewGuid(),
+                Quantity = i
+            };
+
+            bus.InvokeCommand(command);
+
+            Console.WriteLine($"Invoked production of item with ID {command.ItemId}");
+        }
+
+        private static IBus CreateBus(IWindsorContainer container, string hostName)
+        {
+            Uri host = new Uri(hostName);
+
+            var connectionDescriptor = new MassTransitBusConnectionDescriptor(host, "test-1", "test");
+            var builder = new MassTransitBusBuilder(connectionDescriptor);
+
+            IBus bus = builder.Build(c =>
+            {
+                c.SetContainer(container);
+                c.SubscribeToEvent<ItemProduced>(async ctx => Console.WriteLine($"{ctx.Message.Quantity} of {ctx.Message.ItemId} produced."));
+            });
+
+            return bus;
+        }
+
+        static IWindsorContainer CreateContainer()
+        {
+            IWindsorContainer container = new WindsorContainer();
+            container.Install(new DefaultHandlerInstaller());
+
+            return container;
+        }
+    }
+}
