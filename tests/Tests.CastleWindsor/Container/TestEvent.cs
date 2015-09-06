e5f55192c51fabd9b8311a239f343ea919eb9002
Renato Golia <kralizek@gmail.com>
2015-09-06 22:02:47 +0200

    Added test project for CastleWindsor

---
+++ tests/Tests.CastleWindsor/Container/TestEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Nybus;
+
+namespace Tests.Container
+{
+    public class TestEvent : IEvent
+    {
+        
+    }
+
+    public class TestEventHandler : IEventHandler<TestEvent>
+    {
+        public async Task Handle(EventContext<TestEvent> eventMessage)
+        {
+            
+        }
+    }
+}
