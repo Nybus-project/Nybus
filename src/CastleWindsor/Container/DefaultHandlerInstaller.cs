a95b2f526e7e5e3a1de43774660b6f7ed5198799
Renato Golia <kralizek@gmail.com>
2015-09-07 23:29:48 +0200

    Added possibility to specifiy the path where assemblies will be saved when constructing a DefaultHandlerInstaller.

--- src/CastleWindsor/Container/DefaultHandlerInstaller.cs
+++ src/CastleWindsor/Container/DefaultHandlerInstaller.cs
@@ -7,10 +7,26 @@ namespace Nybus.Container
 {
     public class DefaultHandlerInstaller : IWindsorInstaller
     {
+        private readonly string _directory;
+
+        /// <summary>
+        /// A Windsor Installer that will automatically register all the handlers in the AppDomain's base directory
+        /// </summary>
+        public DefaultHandlerInstaller() : this(AppDomain.CurrentDomain.BaseDirectory) { }
+
+        /// <summary>
+        /// A Windsor Installer that will automatically register all the handlers in the <param name="directory"></param> directory
+        /// </summary>
+        /// <param name="directory">The directory to scan for handlers to be registered</param>
+        public DefaultHandlerInstaller(string directory)
+        {
+            if (directory == null) throw new ArgumentNullException(nameof(directory));
+            _directory = directory;
+        }
+
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            var filter = new AssemblyFilter(currentDirectory);
+            var filter = new AssemblyFilter(_directory);
 
             container.Register(Classes.FromAssemblyInDirectory(filter)
                 .BasedOn(typeof(ICommandHandler<>))
