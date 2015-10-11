1ea20524c198e2f78521b8d02370f4bfbcf100d9
Renato Golia <kralizek@gmail.com>
2015-10-11 15:02:55 +0200

    Castle.Windsor: now requires Core

---
+++ src/CastleWindsor/WindsorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Castle.MicroKernel;
+using Nybus.Configuration;
+using Nybus.Container;
+
+namespace Nybus
+{
+    public static class WindsorExtensions
+    {
+        public static void UseCastleWindsor(this NybusOptions options, IKernel kernel)
+        {
+            options.Container = new WindsorBusContainer(kernel);
+        }
+         
+    }
+}
