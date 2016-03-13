d7b6babe2ac345b9d23842dbb1c6669f40b442f7
Renato Golia <golia.renato@gmail.com>
2016-03-13 12:47:28 +0100

    Added LoggerFactoryExtensions
    CreateCurrentClassLogger creates a logger using the calling class full name

---
+++ src/Interfaces/Logging/LoggerFactoryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Nybus.Logging
+{
+    public static class LoggerFactoryExtensions
+    {
+        /// <summary>
+        /// Gets the logger with the name of the current class. 
+        /// </summary>
+        /// <returns>The logger.</returns>
+        /// <remarks>This is a slow-running method. Make sure you're not doing this in a loop.</remarks>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static ILogger CreateCurrentClassLogger(this ILoggerFactory loggerFactory)
+        {
+            var frame = new StackFrame(1, false);
+
+            string loggerName = frame.GetMethod().DeclaringType.FullName;
+
+            return loggerFactory.CreateLogger(loggerName);
+        }
+    }
+}
