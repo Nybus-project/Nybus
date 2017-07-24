eb0bb2ce6083148fd44538056a1ecba1a4046128
Renato Golia <golia.renato@gmail.com>
2017-07-24 11:01:36 +0200

    Created RabbitMQBusEngine for Nybus.RabbitMQ

---
+++ src/Nybus.RabbitMQ/RabbitMQBusEngine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nybus
+{
+    public class RabbitMQBusEngine : IBusEngine
+    {
+        public RabbitMQBusEngine()
+        {
+
+        }
+
+        public IObservable<Message> Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IBusEngine.SendCommandAsync<TCommand>(CommandMessage<TCommand> message)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IBusEngine.SendEventAsync<TEvent>(EventMessage<TEvent> message)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBusEngine.SubscribeToCommand<TCommand>()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBusEngine.SubscribeToEvent<TEvent>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
