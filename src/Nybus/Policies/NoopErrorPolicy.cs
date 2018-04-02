d2c33d12f851f69efc70d68242101699268ae74e
Renato Golia <golia.renato@gmail.com>
2018-04-02 13:55:08 +0200

    Improved ErrorPolicy management

---
+++ src/Nybus/Policies/NoopErrorPolicy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Nybus.Policies {
+    public class NoopErrorPolicy : IErrorPolicy
+    {
+        public async Task HandleError<TCommand>(IBusEngine engine, Exception exception, CommandMessage<TCommand> message)
+            where TCommand : class, ICommand
+        {
+            await engine.NotifyFail(message).ConfigureAwait(false);
+        }
+
+        public async Task HandleError<TEvent>(IBusEngine engine, Exception exception, EventMessage<TEvent> message)
+            where TEvent : class, IEvent
+        {
+            await engine.NotifyFail(message).ConfigureAwait(false);
+        }
+    }
+}
--- src/Nybus/Policies/RetryErrorPolicy.cs
+++ src/Nybus/Policies/RetryErrorPolicy.cs
@@ -7,21 +7,6 @@ using System.Threading.Tasks;
 
 namespace Nybus.Policies
 {
-    public class NoopErrorPolicy : IErrorPolicy
-    {
-        public async Task HandleError<TCommand>(IBusEngine engine, Exception exception, CommandMessage<TCommand> message)
-            where TCommand : class, ICommand
-        {
-            await engine.NotifyFail(message).ConfigureAwait(false);
-        }
-
-        public async Task HandleError<TEvent>(IBusEngine engine, Exception exception, EventMessage<TEvent> message)
-            where TEvent : class, IEvent
-        {
-            await engine.NotifyFail(message).ConfigureAwait(false);
-        }
-    }
-
     public class RetryErrorPolicy : IErrorPolicy
     {
         private readonly ILogger<RetryErrorPolicy> _logger;
