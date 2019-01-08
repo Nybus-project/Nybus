d3a8f8a0174bceb10871deffc20c75bc98688593
Renato Golia <golia.renato@gmail.com>
2019-01-08 22:58:39 +0100

    Extract InMemoryBusEngine into its own project

---
+++ src/engines/Nybus.Engine.InMemory/InMemoryConfiguratorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nybus.Configuration;
+
+namespace Nybus
+{
+    public static class InMemoryConfiguratorExtensions
+    {
+        public static void UseInMemoryBusEngine(this INybusConfigurator configurator)
+        {
+            configurator.UseBusEngine<InMemoryBusEngine>();
+        }
+    }
+}
---
+++ tests/Tests.Nybus.Engine.InMemory/AutoMoqDataAttribute.cs
@@ -0,0 +1,51 @@
+using System;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.NUnit3;
+
+namespace Tests
+{
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute() : base(CreateFixture)
+        {
+            
+        }
+
+        private static IFixture CreateFixture()
+        {
+            IFixture fixture = new Fixture();
+
+            fixture.Customize(new AutoMoqCustomization
+            {
+                ConfigureMembers = true,
+                GenerateDelegates = true
+            });
+
+            fixture.Freeze<IServiceProvider>();
+
+            return fixture;
+        }
+    }
+
+    public class InlineAutoMoqDataAttribute : InlineAutoDataAttribute
+    {
+        public InlineAutoMoqDataAttribute(params object[] args) : base(CreateFixture, args)
+        {
+            
+        }
+
+        private static IFixture CreateFixture()
+        {
+            IFixture fixture = new Fixture();
+
+            fixture.Customize(new AutoMoqCustomization
+            {
+                ConfigureMembers = true,
+                GenerateDelegates = true
+            });
+
+            return fixture;
+        }
+    }
+}
