5eb3ddc927db1ef266e2292dec0b1e9dbd2e9e7e
Renato Golia <golia.renato@gmail.com>
2019-02-13 20:19:26 +0100

    Add Nybus.Extensions.Hosting.TopShelf package (#70)
    - TopShelf helper classes in new package
    - Sample NetFxWindowsService modified

---
+++ src/extensions/hosting/Nybus.Extensions.TopShelf/HostConfiguratorExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Topshelf;
+using Topshelf.HostConfigurators;
+using Topshelf.ServiceConfigurators;
+
+namespace Nybus
+{
+    public static class HostConfiguratorExtensions
+    {
+        public static HostConfigurator UseStartup<TStartup>(this HostConfigurator configurator)
+            where TStartup : Startup, new()
+        {
+            return configurator.Service<IBusHost>(UseStartup<TStartup>);
+        }
+
+        private static void UseStartup<TStartup>(ServiceConfigurator<IBusHost> configurator)
+            where TStartup : Startup, new()
+        {
+            var startup = new TStartup();
+
+            configurator.ConstructUsing(settings =>
+            {
+                var configurationBuilder = new ConfigurationBuilder();
+
+                startup.ConfigureAppConfiguration(configurationBuilder);
+
+                var configuration = configurationBuilder.Build();
+
+                var context = new StartupContext(settings, configuration);
+
+                var services = new ServiceCollection();
+                services.AddSingleton(configuration);
+
+                startup.ConfigureServices(context, services);
+
+                services.AddLogging(builder => startup.ConfigureLogging(context, builder));
+
+                var serviceProvider = services.BuildServiceProvider();
+
+                return startup.ConstructService(context, serviceProvider);
+            });
+
+            configurator.WhenStarted(startup.OnStart);
+
+            configurator.WhenStopped(startup.OnStop);
+        }
+    }
+}
---
+++ src/extensions/hosting/Nybus.Extensions.TopShelf/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Topshelf;
+using Topshelf.Runtime;
+
+namespace Nybus
+{
+    public abstract class Startup
+    {
+        public virtual void ConfigureAppConfiguration(IConfigurationBuilder configurationBuilder) { }
+
+        public virtual void ConfigureServices(StartupContext context, IServiceCollection services) { }
+
+        public virtual void ConfigureLogging(StartupContext context, ILoggingBuilder logging) { }
+
+        public abstract IBusHost ConstructService(StartupContext context, IServiceProvider serviceProvider);
+
+        public virtual bool OnStart(IBusHost host, HostControl control)
+        {
+            host.StartAsync().GetAwaiter().GetResult();
+
+            return true;
+        }
+
+        public virtual bool OnStop(IBusHost host, HostControl control)
+        {
+            host.StopAsync().GetAwaiter().GetResult();
+
+            return true;
+        }
+    }
+
+    public class StartupContext
+    {
+        public StartupContext(HostSettings settings, IConfigurationRoot configuration)
+        {
+            Settings = settings ?? throw new ArgumentNullException(nameof(settings));
+            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public HostSettings Settings { get; }
+
+        public IConfigurationRoot Configuration { get; }
+    }
+}
