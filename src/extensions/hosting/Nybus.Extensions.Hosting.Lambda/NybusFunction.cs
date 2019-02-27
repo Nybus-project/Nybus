94485cf1c4757cb6e75a2bea8eda0dc367635a95
Renato Golia <golia.renato@gmail.com>
2019-02-28 00:23:52 +0100

    Add Lambda hosting package

---
+++ src/extensions/hosting/Nybus.Extensions.Hosting.Lambda/NybusFunction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Kralizek.Lambda;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Nybus
+{
+    public abstract class NybusFunction<TInput> : Function
+    {
+        protected NybusFunction()
+        {
+            _busHost = ServiceProvider.GetRequiredService<IBusHost>();
+        }
+
+        private readonly IBusHost _busHost;
+
+        public async Task FunctionHandlerAsync(TInput input, ILambdaContext context)
+        {
+            await _busHost.StartAsync().ConfigureAwait(false);
+
+            using (var scope = ServiceProvider.CreateScope())
+            {
+                var handler = scope.ServiceProvider.GetService<Kralizek.Lambda.IEventHandler<TInput>>();
+
+                if (handler == null)
+                {
+                    Logger.LogCritical($"No IEventHandler<{typeof(TInput).Name}> could be found.");
+                    throw new InvalidOperationException($"No IEventHandler<{typeof(TInput).Name}> could be found.");
+                }
+
+                Logger.LogInformation("Invoking handler");
+                await handler.HandleAsync(input, context).ConfigureAwait(false);
+            }
+
+            await _busHost.StopAsync().ConfigureAwait(false);
+        }
+
+        protected void RegisterHandler<THandler>(IServiceCollection services) where THandler : class, Kralizek.Lambda.IEventHandler<TInput>
+        {
+            services.AddTransient<Kralizek.Lambda.IEventHandler<TInput>, THandler>();
+        }
+    }
+}
