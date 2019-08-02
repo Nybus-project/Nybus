7c98e7fbd19de13cf5ad5a4d341d48ccb1fe95cd
Renato Golia <golia.renato@gmail.com>
2019-08-02 19:39:08 +0200

    Initial commit

---
+++ src/engines/Nybus.Engine.MassTransit/MassTransit/MassTransitBusEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Nybus.MassTransit
+{
+    public class MassTransitBusEngine : IBusEngine
+    {
+        public Task<IObservable<Message>> StartAsync() => throw new NotImplementedException();
+
+        public Task StopAsync() => throw new NotImplementedException();
+
+        public void SubscribeToCommand<TCommand>()
+            where TCommand : class, ICommand
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeToEvent<TEvent>()
+            where TEvent : class, IEvent
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMessageAsync(Message message) => throw new NotImplementedException();
+
+        public Task NotifySuccessAsync(Message message) => throw new NotImplementedException();
+
+        public Task NotifyFailAsync(Message message) => throw new NotImplementedException();
+    }
+}
